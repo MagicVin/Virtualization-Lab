c39b5c416510dafa656cf142c3227dcacd209e86
Vin <v.xin.zhang@gmail.com>
2022-08-17 01:12:54 +0800

    Update qemuctl.sh

--- qemuctl.sh
+++ qemuctl.sh
@@ -25,7 +25,7 @@ modinstall() {
 		)
 	echo "Modules installing ..."
 	for mod in ${modlist[@]} ;{
-		modprobe $mod 2>/dev/null  && {
+		echo modprobe $mod && modprobe $mod 2>/dev/null  && {
 			fp install $mod successfully 
 		} || {
 			fp install $mod failed
@@ -39,19 +39,11 @@ drvdetach() {
 		[ -f /sys/bus/pci/devices/*${bus}/driver/unbind ] && {
 			busfpath="`ls -d /sys/bus/pci/devices/*${bus}`"
 			busfpath=${busfpath##*/}
+			echo "echo $busfpath > /sys/bus/pci/devices/${busfpath}/driver/unbind" && 
 			echo $busfpath > /sys/bus/pci/devices/${busfpath}/driver/unbind && {
 				fp detach $busfpath successfully
 			} || {
 				fp detach $busfpath failed
-				exit -1
-			}
-			drvname="`ls /sys/bus/pci/devices/${busfpath}/driver -l`"
-			drvname=${drvname##*/}
-			vender_dev=`lspci -s $bus -n | sed -n 's/^.*\([[:alnum:]]\{4\}\)\:\([[:alnum:]]\{4\}\).*/\1 \2/p'`
-			echo $vender_dev > /sys/bus/pci/drivers/${drvname}/remove_id && {
-				fp remove $vender_dev successfully
-			} || {
-				fp remove $vender_dev failed 
 				exit -1
 			}
 		} || {
@@ -64,7 +56,8 @@ drvattach() {
 	fp vfio devices list: `ls /dev/vfio | xargs`
 	for bus in ${bus_ids[@]} ;{
 		vender_dev=`lspci -s $bus -n | sed -n 's/^.*\([[:alnum:]]\{4\}\)\:\([[:alnum:]]\{4\}\).*/\1 \2/p'`
-		[[ $vender_dev ]] && echo $vender_dev > /sys/bus/pci/drivers/vfio-pci/new_id && fp attach $bus successfully
+		[[ $vender_dev ]] && echo "echo $vender_dev > /sys/bus/pci/drivers/vfio-pci/new_id" && \
+		echo $vender_dev > /sys/bus/pci/drivers/vfio-pci/new_id && fp attach $bus successfully
 	}
 	fp vfio devices list: `ls /dev/vfio | xargs`
 }
@@ -84,11 +77,52 @@ devpthr() {
 	}
 }
 
+diskcreate() {
+	# for performance: use raw
+	qemu-img create -f raw -o preallocation=full /data/img/win10-disk0.img 60G
+}
+
+hugepagecreate() {
+
+
+}
+
+
+cmdrun() {
+	cmd=(
+		taskset -c 14-17,32-35
+		qemu-system-x86_64 
+		-enable-kvm
+		-machine type=q35, accel=kvm
+		-nic none
+		-vga none
+		-serial none
+		-parallel none 
+		-nographic
+		-cpu host,kvm=off
+		-rtc base=localtime,clock=host
+		-daemonize
+		-k en-us
+		-m 16G
+		-smp cpus=16,cores=16,sockets=1
+		-device pcie-root-port,chassis=0,id=pci.0,multifunction=on
+		-device vfio-pci,host=65:00.0,bus=pci.0
+		-device pcie-root-port,chassis=1,id=pci.1,multifunction=on
+		-device vfio-pci,host=65:00.1,bus=pci.1
+		-device pcie-root-port,chassis=2,id=pci.2,multifunction=on
+		-device vfio-pci,host=b3:00.0,bus=pci.2
+		-drive id=disk0,if=virtio,cache=none,format=raw,file=/data/img/win10-disk0.img
+		-drive file=/data/iso/Windows10-Jun19-2022.iso,index=1,media=cdrom
+		-boot dc
+
+	)
+
+
+}
 
 main() {
 	modinstall
 	devpthr
-
 }
 
 main
